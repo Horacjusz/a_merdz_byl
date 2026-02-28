9057850ee8187f88745857ed98b9fe55fe11fe6c
PSielchan <147639332+PSielchan@users.noreply.github.com>
2026-02-28 22:28:41 +0100

    working wires

--- game/scripts/wire.gd
+++ game/scripts/wire.gd
@@ -1,6 +1,8 @@
 extends TextureButton
 
 var holding = false
+var is_right = false
+var is_in = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,7 +22,23 @@ func _process(delta: float) -> void:
 
 func _on_button_down() -> void:
 	holding = true
-
+	is_right = false
 
 func _on_button_up() -> void:
 	holding = false
+	if is_in:
+		is_right=true
+		#print("jest")
+
+
+
+func _on_mouse_entered() -> void:
+	is_in = true
+
+
+func _on_mouse_exited() -> void:
+	is_in = false
+
+
+func _on_redslot_mouse_exited() -> void:
+	pass # Replace with function body.
