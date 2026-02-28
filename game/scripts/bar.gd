1e8f5b1da8427af78620c9ec310987ecf6177435
Paweł Prus <68601238+Horacjusz@users.noreply.github.com>
2026-03-01 00:14:32 +0100

    Pawel (#16)
    * Backup
    
    * Backup of tileset level test
    
    * backup
    
    * Sounds and tilemap?
    
    * Footsteps done (now animations)
    
    * Footsteps backup
    
    * Player backup before rewrite
    
    * player done and backup
    
    * Sanity bar done

--- game/scripts/bar.gd
+++ game/scripts/bar.gd
@@ -1,35 +1,47 @@
-extends Sprite2D
+extends Node2D
 
-@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
+@onready var inside: Sprite2D = $Inside
 
-var barpos=1
+var Y_TRAVEL := 120.0 * scale.y
+var CLIP_HEIGHT := 50 * scale.y
+var BAR_FREEZED = false
 
-# Called when the node enters the scene tree for the first time.
+@export var travel_rate := 0.1
+var _target_percentage: float = 0.0
+var value: float = 0.0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_set_position_to_value()
+	_update_clip()
+	
+func freeze() :
+	BAR_FREEZED = true
+	
+func unfreeze() :
+	BAR_FREEZED = false
 
+func increase(increase_value: float) -> void:
+	unfreeze()
+	_target_percentage = clamp(_target_percentage + increase_value, 0.0, 1.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func start() :
+	increase(1)
+
+func _set_position_to_value() -> void:
+	inside.position.y = Y_TRAVEL * (1.0 - value)
+
+func _update_clip() -> void:
+	var clip_line_y := global_position.y + CLIP_HEIGHT
+
+	var mat := inside.material as ShaderMaterial
+	if mat:
+		mat.set_shader_parameter("clip_y", clip_line_y)
+
 func _process(delta: float) -> void:
-	pass
-
-func next_bar():
-	if barpos==1:
-		animation_player.play("lv1")
-		barpos+=1
-	elif barpos==2:
-		animation_player.play("lv2")
-		barpos+=1
-	elif barpos==3:
-		animation_player.play("lv3")
-		barpos+=1
-	elif barpos==4:
-		animation_player.play("lv4")
-		barpos+=1
-	elif barpos==5:
-		animation_player.play("RESET")
-		barpos=1
-
-func _unhandled_input(event):
-	if event.is_action_pressed("spacja"):
-		next_bar()
+	if not BAR_FREEZED :
+		var difference := _target_percentage - value
+		if difference != 0.0:
+			value = clamp(value + travel_rate * delta * sign(difference), 0.0, 1.0)
+			_set_position_to_value()
+		_update_clip()
+	
