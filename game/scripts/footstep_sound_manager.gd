8aaf7967318fd57ccf594544b53ffe1aae48f622
Horacjusz <pawel.prus05@gmail.com>
2026-02-28 18:24:23 +0100

    Footsteps backup

--- game/scripts/footstep_sound_manager.gd
+++ game/scripts/footstep_sound_manager.gd
@@ -3,10 +3,12 @@ extends Node
 var tilemaps: Array[TileMapLayer] = []
 
 const footstep_sounds = {
-	"stone": [
+	"default": [
 		preload("res://assets/sounds/Footsteps/data_pion-st1-footstep-sfx-323053.mp3"),
 		preload("res://assets/sounds/Footsteps/data_pion-st2-footstep-sfx-323055.mp3"),
 		preload("res://assets/sounds/Footsteps/data_pion-st3-footstep-sfx-323056.mp3"),
+	],
+	"stone": [
 	],
 	"bush": [
 		preload("res://assets/sounds/Item/item-pickup.mp3")
@@ -16,24 +18,27 @@ const footstep_sounds = {
 func add_tilemap(tilemap: TileMapLayer) -> void :
 	tilemaps.push_back(tilemap)
 
-
 func play_footstep(position: Vector2) -> void:
-	var tile_data = []
+	var tile_data = "default"
 	for tilemap in tilemaps :
 		var tile_position = tilemap.local_to_map(position)
 		var data = tilemap.get_cell_tile_data(tile_position)
 		if data :
-			tile_data.push_back(data)
-			
-	if tile_data.size() > 0 :
+			tile_data = data
+	
+	var footstep_sound = footstep_sounds["default"][0]
+	
+	if tile_data != "default" :
 		var tile_type = tile_data.back().get_custom_data("footstep_sound")
 		
 		if footstep_sounds.has(tile_type) :
-			var audio_player = AudioStreamPlayer2D.new()
-			audio_player.stream = footstep_sounds[tile_type].pick_random()
-			get_tree().root.add_child(audio_player)
-			audio_player.global_position = position
+			footstep_sound = footstep_sounds[tile_type].pick_random()
 			
-			audio_player.play()
-			await audio_player.finished
-			audio_player.queue_free()
+	var audio_player = AudioStreamPlayer2D.new()
+	audio_player.stream = footstep_sound
+	get_tree().root.add_child(audio_player)
+	audio_player.global_position = position
+	
+	audio_player.play()
+	await audio_player.finished
+	audio_player.queue_free()
--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,27 +1,55 @@
 extends CharacterBody2D
 
-@onready var sprite: AnimatedSprite2D = $Sprite2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 const SPEED = 100
 
+# seconds
+var STEP_DURATION = 0.3
+
 enum MOVE_DIRECTION {front, right, back, left}
-var direction = MOVE_DIRECTION.front
+enum PLAYER_MOVE_STATE {idle, moving}
 
-func _play_idle():
-	match direction:
-		MOVE_DIRECTION.right:
-			sprite.play("right")
-		MOVE_DIRECTION.left:
-			sprite.play("left")
-		MOVE_DIRECTION.front:
-			sprite.play("front")
-		MOVE_DIRECTION.back:
-			sprite.play("back")
-			
+var direction
+var prev_timer
+
+var current_player_move_state
+
+var previous_frame: int
+
+var ANIMATION_MAP = {
+	MOVE_DIRECTION.front: {
+		PLAYER_MOVE_STATE.idle: ["idle_front", 0],
+		PLAYER_MOVE_STATE.moving: ["front", 0]
+	},
+	MOVE_DIRECTION.right: {
+		PLAYER_MOVE_STATE.idle: ["idle_right", 0],
+		PLAYER_MOVE_STATE.moving: ["right", 0]
+	},
+	MOVE_DIRECTION.back: {
+		PLAYER_MOVE_STATE.idle: ["idle_back", 0],
+		PLAYER_MOVE_STATE.moving: ["back", 0]
+	},
+	MOVE_DIRECTION.left: {
+		PLAYER_MOVE_STATE.idle: ["idle_left", 0],
+		PLAYER_MOVE_STATE.moving: ["left", 0]
+	}
+}
 
 
-func _physics_process(_delta: float) -> void:
+func _update_animation(delta: float) -> void:
+	sprite.play(ANIMATION_MAP[direction][current_player_move_state][0])
+	sprite.frame_progress = ANIMATION_MAP[direction][current_player_move_state][1]
+	
+	previous_frame = -1
+
+func _physics_process(delta: float) -> void:
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	var previous_player_move_state = current_player_move_state
+	var previous_direction = direction
+	current_player_move_state = PLAYER_MOVE_STATE.moving
 	
 	if input_direction[1] < 0:
 		direction = MOVE_DIRECTION.front
@@ -31,12 +59,33 @@ func _physics_process(_delta: float) -> void:
 		direction = MOVE_DIRECTION.right
 	elif input_direction[0] < 0:
 		direction = MOVE_DIRECTION.left
+	else :
+		current_player_move_state = PLAYER_MOVE_STATE.idle
 	
-	_play_idle()
+	if (previous_direction != direction) or (previous_player_move_state != current_player_move_state) :
+		_update_animation(delta)
+	
+	if current_player_move_state == PLAYER_MOVE_STATE.moving :
+		if prev_timer < timer.time_left :
+			if previous_frame != sprite.frame :
+				previous_frame = sprite.frame
+				_play_footstep()
+	prev_timer = timer.time_left
+				
+	ANIMATION_MAP[direction][current_player_move_state][1] += delta
+
+	print(position)
 		
 	velocity = input_direction * SPEED
 	move_and_slide()
-	#_play_footstep()
+	
+
+func _ready() -> void :
+	timer.start(STEP_DURATION)
+	current_player_move_state = PLAYER_MOVE_STATE.idle
+	direction = MOVE_DIRECTION.front
+	prev_timer = float('inf')
+	_update_animation(0)
 
 func _play_footstep() -> void :
 	FootstepSoundManager.play_footstep(global_position)
