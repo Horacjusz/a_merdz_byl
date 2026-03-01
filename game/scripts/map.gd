39e5446d586df538c56bef58fa990eb7942d7c35
Eryk Olejarz <olejarzeryk@gmail.com>
2026-03-01 14:33:02 +0100

    Add level 2

--- game/scripts/map.gd
+++ game/scripts/map.gd
@@ -1,17 +1,26 @@
 extends TextureButton
 
 
-signal map_clicked
+signal map_opened
+signal map_closed
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var isOpened: bool = false
 
-func change(another_world: bool) :
-	if another_world:
-		animation_player.play("zwin")
-	else:
-		animation_player.play("rozwin")
+
+func open() -> void:
+	isOpened = true
+	animation_player.play("rozwin")
+	
+
+func close() -> void:
+	isOpened = false
+	animation_player.play("zwin")
 
 
 func _on_button_down() -> void:
-	map_clicked.emit()
+	if isOpened:
+		map_closed.emit()
+	else:
+		map_opened.emit()
