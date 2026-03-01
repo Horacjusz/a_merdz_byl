501106970fd408eb0636abb88577e6e29b8fe5e3
Eryk666 <olejarzeryk@gmail.com>
2026-03-01 11:11:48 +0100

    Fix camera and simplify some scripts

--- game/scripts/map.gd
+++ game/scripts/map.gd
@@ -1,27 +1,17 @@
 extends TextureButton
 
+
 signal map_clicked
 
-@onready var level_container: Node2D = $"../../World/LevelContainer"
-
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var bar: Node2D = $"../Bar"
 
-var another_world=false
-
-func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	pass
-	
 
 func change(another_world: bool) :
 	if another_world:
 		animation_player.play("zwin")
 	else:
 		animation_player.play("rozwin")
+
 
 func _on_button_down() -> void:
 	map_clicked.emit()
