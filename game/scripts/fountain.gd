807aaaa9e48ee389982815cf4c1d4cad52edbe43
Horacjusz <pawel.prus05@gmail.com>
2026-03-01 18:39:59 +0100

    HOTFIX 2

--- game/scripts/fountain.gd
+++ game/scripts/fountain.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var sip_sound: AudioStreamPlayer2D = $Sip_sound
 
 @export var sip_value = 0.3
+var sip_taken = false
 
 func _ready() -> void:
 	background_music.play()
@@ -13,15 +14,15 @@ func interact(player: Player) :
 	_take_a_sip(player)
 
 func _on_range_body_entered(body: Node2D) -> void:
-	if body is Player :
+	if body is Player and not sip_taken :
 		f_button.show()
 
 func _on_range_body_exited(body: Node2D) -> void:
-	if body is Player :
+	if body is Player and not sip_taken :
 		f_button.hide()
 
 func _take_a_sip(player) -> void :
-	if f_button.visible :
+	if f_button.visible and not sip_taken :
 		sip_sound.play()
 		player.take_a_sip(sip_value)
 		f_button.hide()
