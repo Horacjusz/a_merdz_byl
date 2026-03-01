a946f5453d0542555cb6f1b5879e8fef57930f5d
Horacjusz <pawel.prus05@gmail.com>
2026-03-01 14:54:13 +0100

    backup

--- game/scripts/map.gd
+++ game/scripts/map.gd
@@ -5,13 +5,25 @@ signal map_clicked
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var screen_black = false
+
+func toogle_black_screen() :
+	if screen_black :
+		animation_player.play("unblacken")
+	else :
+		animation_player.play("blacken")
 
 func change(another_world: bool) :
+	animation_player.play("blacken")
+	await animation_player.animation_finished
+	
 	if another_world:
 		animation_player.play("zwin")
 	else:
 		animation_player.play("rozwin")
-
+	
+	await animation_player.animation_finished
+	animation_player.play("unblacken")
 
 func _on_button_down() -> void:
 	map_clicked.emit()
